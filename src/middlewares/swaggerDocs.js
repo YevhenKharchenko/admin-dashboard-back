910a816341f085900c738651452670e7e1e2c169
YevhenKharchenko <evgenvalerych@gmail.com>
2024-11-10 00:20:01 +0200

    fix docs path

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -5,6 +5,8 @@ import fs from 'node:fs';
 import { SWAGGER_PATH } from '../constants/index.js';
 
 export const swaggerDocs = () => {
+  console.log(SWAGGER_PATH);
+
   try {
     const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
